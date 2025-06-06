030fa90e4ab638bd5924a101165b6ea640decad8
phangb <taolaai2468@gmail.com>
2025-06-06 15:53:03 +0700

    add controller and route bookings

--- backend/resources/views/layouts/sidebar.blade.php
+++ backend/resources/views/layouts/sidebar.blade.php
@@ -24,7 +24,7 @@
         <a href="#" class="nav-link dropdown-toggle {{ request()->is('booking.html') || request()->is('contract.html') ? 'active' : '' }}" data-bs-toggle="dropdown"><i class="fa fa-calendar-check me-2"></i>Đặt & Hợp đồng</a>
         <div class="dropdown-menu bg-transparent border-0">
             <a href="booking.html" class="dropdown-item {{ request()->is('booking.html') ? 'active' : '' }}">Lịch xem phòng</a>
-            <a href="booking.html" class="dropdown-item {{ request()->is('booking.html') ? 'active' : '' }}">Đặt phòng</a>
+            <a href="{{ route('bookings.index') }}" class="dropdown-item {{ request()->routeIs('bookings.*') ? 'active' : '' }}">Đặt phòng</a>
             <a href="contract.html" class="dropdown-item {{ request()->is('contract.html') ? 'active' : '' }}">Hợp đồng</a>
         </div>
     </div>
