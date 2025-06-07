99cb4114f131fd5ae202cb19b7c626786492a87c
hailam2706 <9hailam.nh@gmail.com>
2025-06-07 22:05:15 +0700

    fix send mail booking

--- backend/app/Mail/BookingRejected.php
+++ backend/app/Mail/BookingRejected.php
@@ -3,17 +3,12 @@
 namespace App\Mail;
 
 use App\Models\Booking;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
-use Illuminate\Queue\SerializesModels;
 
-class BookingRejected extends Mailable implements ShouldQueue
+class BookingRejected extends Mailable
 {
-    use Queueable, SerializesModels;
-
     public $booking;
     public $rejectionReason;
 
