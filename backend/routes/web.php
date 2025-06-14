3d112eb882f1100df293b3029d62d16a77300c74
Phan Quoc Duong <phanquocduong.070905@gmail.com>
2025-06-14 11:23:55 +0700

    Fix conflict

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -137,6 +137,4 @@ Route::middleware('admin')->group(function () {
         Route::get('/{id}', [ContractController::class, 'show'])->name('show');
         Route::match(['put', 'patch'],'/{id}/update-status', [ContractController::class, 'updateStatus'])->name('updateStatus');
     });
-
-    Route::patch('/schedules/ajax-update/{id}', [ScheduleController::class, 'ajaxUpdateStatus'])->name('schedules.ajaxUpdateStatus');
 });
