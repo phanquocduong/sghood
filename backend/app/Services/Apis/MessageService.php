75c66b5d2b2b118416c04684c38550a36bfc0ef2
HanKim <l3monsu@gmail.com>
2025-06-20 10:44:12 +0700

    da merge code caophuoc

---
+++ backend/app/Services/Apis/MessageService.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Services\Apis;
+
+use App\Models\Message;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class MessageService
+{
+    public function sendMessage(array $data)
+    {
+    $data['sender_id'] = Auth::id();
+
+    \Log::info('Sender ID:', ['id' => $data['sender_id']]); // thêm dòng này
+
+    return Message::create($data);
+    }
+
+    public function getChatHistory($userId)
+    {
+        $authId = Auth::id();
+
+        return Message::where(function ($query) use ($authId, $userId) {
+            $query->where('sender_id', $authId)->where('receiver_id', $userId);
+        })->orWhere(function ($query) use ($authId, $userId) {
+            $query->where('sender_id', $userId)->where('receiver_id', $authId);
+        })->orderBy('created_at', 'asc')->get();
+    }
+
+    public function getUsersChattedWithAdmin()
+    {
+        $adminId = 1;
+
+        $userIds = Message::where('sender_id', $adminId)
+            ->orWhere('receiver_id', $adminId)
+            ->pluck('sender_id')
+            ->merge(
+                Message::where('sender_id', $adminId)
+                    ->orWhere('receiver_id', $adminId)
+                    ->pluck('receiver_id')
+            )
+            ->unique()
+            ->filter(fn($id) => $id != $adminId)
+            ->values();
+
+        return User::whereIn('id', $userIds)->get();
+    }
+}
