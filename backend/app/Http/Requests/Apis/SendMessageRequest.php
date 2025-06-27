5ed2cf56b426ffdaca558b499a3e44a3dc553bca
HanKim <l3monsu@gmail.com>
2025-06-27 14:18:39 +0700

    thg l phuoc

--- backend/app/Http/Requests/Apis/SendMessageRequest.php
+++ backend/app/Http/Requests/Apis/SendMessageRequest.php
@@ -19,18 +19,17 @@ class SendMessageRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'receiver_id' => 'required|exists:users,id',
-            'message' => 'required|string',
+            'receiver_id' => 'nullable|exists:users,id',
+            
         ];
     }
-
+    
     public function messages(): array
     {
         return [
-            'receiver_id.required' => 'Vui lòng chọn người nhận',
+        
             'receiver_id.exists' => 'Người nhận không tồn tại',
-            'message.required' => 'Vui lòng nhập tin nhắn',
-            'message.string' => 'Tin nhắn phải là chuỗi ký tự',
+           
         ];
     }
 }
